2dbc47f468fdc4cb31283a0edd925971bd976a11
Nicholas Davidson <nicholasdavidson7@gmail.com>
2023-09-25 13:29:25 -0500

    Fixed rides filter inputs too small on mobile

--- resources/views/rides/index.blade.php
+++ resources/views/rides/index.blade.php
@@ -3,18 +3,18 @@
         class="flex-grow-1 @if ($rides->isEmpty()) d-flex justify-content-center align-items-center @else container-fluid py-3 @endif">
         @if ($rides->count())
             <form action="#" method="get" class="mb-3">
-                <div class="row align-items-center">
-                    <div class="col">
+                <div class="row justify-content-center align-items-center row-gap-2">
+                    <div class="col-auto">
                         {{-- <label class="form-label" for="origin-city">Origin City</label> --}}
                         <input type="text" class="form-control" name="origin-city" id="origin-city"
                             placeholder="Origin City" value="{{ request('origin-city') }}" />
                     </div>
-                    <div class="col">
+                    <div class="col-auto">
                         {{-- <label class="form-label" for="destination-city">Destination City</label> --}}
                         <input type="text" class="form-control" name="destination-city" id="destination-city"
                             placeholder="Destination City" value="{{ request('destination-city') }}" />
                     </div>
-                    <div class="col">
+                    <div class="col-auto">
                         <?php date_default_timezone_set('America/New_York'); ?>
                         {{-- <label class="form-label" for="start-date">Date</label> --}}
                         <input type="date" class="form-control" name="start-date" id="start-date"
@@ -29,7 +29,7 @@
                             value="1" @if (request('detours')) checked @endif />
                     </div>
                     <div class="col-auto">
-                        <button type="submit" class="btn btn-uva-ob fs-5 fw-bold">Filter</button>
+                        <button type="submit" class="btn btn-uva-ob fs-6 fw-bold">Filter</button>
                     </div>
                 </div>
             </form>
