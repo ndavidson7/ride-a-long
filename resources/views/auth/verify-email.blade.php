50fb9cbc1c13958b9c16378f21153ced0010a412
Nicholas Davidson <nicholasdavidson7@gmail.com>
2024-01-02 23:26:22 -0600

    More semantic HTML & minor styling improvements to email verification view

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,11 +1,12 @@
 <x-layouts.splash title="Verify email" :entries="[]">
-    <main class="text-center text-white py-auto">
-        <h2>For the safety of all users, you have to verify your email before you can use this site.</h2>
-        <h3>Click the link in the email that was sent to you. If you didn't receive an email, click the button below to
-            resend it.</h3>
-        <form action="{{ route('verification.send') }}" method="POST" class="mt-5">
+    <main class="mx-auto" style="max-width: min(800px, 80vw);">
+        <h1>Check your email!</h1>
+        <p class="fs-4">For the safety of all users, you must verify your email before you can use this site. Click
+            the link in the email that was sent to you. If you didn't receive an email, click the button below to resend
+            it.</p>
+        <form action="{{ route('verification.send') }}" method="POST">
             @csrf
-            <button type="submit" class="btn btn-uva-ow">Resend</button>
+            <button type="submit" class="btn btn-primary">Resend</button>
         </form>
     </main>
 </x-layouts.splash>
