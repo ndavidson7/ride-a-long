57bb874623e8178e1b1e99aba36f15cadf40e7fa
Nicholas Davidson <nicholasdavidson7@gmail.com>
2024-06-22 18:10:02 -0500

    Converted to Laravel Sail for local dev. Configured PHPUnit to use .env.testing by default.

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function test_can_access_signup_page(): void
+    {
+        $response = $this->get('/signup');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_no_users_exist(): void
+    {
+        $this->assertEmpty(User::all());
+    }
+
+    // public function test_can_signup_new_user(): void
+    // {
+    //     $response = $this->post('/signup', [
+    //         'first-name' => 'John',
+    //         'last-name' => 'Doe',
+    //         'email' => 'johndoe@virginia.edu',
+    //         'phone' => '1234567890',
+    //         'password' => 'password',
+    //         'password_confirmation' => 'password',
+    //     ]);
+
+    //     $response->assertRedirect('/email/verify');
+    // }
+}
